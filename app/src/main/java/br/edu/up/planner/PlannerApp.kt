3aa7ba5354919472e941a6f30970ae85a74312a5
MarcelFlrs <127535882+MarcelFlrs@users.noreply.github.com>
2024-09-06 11:07:41 -0300

    .

---
+++ app/src/main/java/br/edu/up/planner/PlannerApp.kt
@@ -0,0 +1,86 @@
+package br.edu.up.planner
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.edu.up.planner.ui.screens.*
+
+@Preview(
+    device = Devices.PIXEL
+)
+@Composable
+fun PlannerApp() {
+
+    val tela = remember {mutableStateOf(1) }
+
+    Surface(modifier = Modifier.fillMaxSize()) {
+
+        Column {
+            Row(
+                Modifier
+                    .height(70.dp)
+                    .fillMaxWidth()
+                    .background(Color(0xFF1AB6FF)),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+
+            ){
+                Text(text = "Planner",
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight(800),
+                    color = Color.White,
+
+                )
+            }
+
+            Row (modifier = Modifier.weight(1f)){
+                when(tela.value){
+                    1 -> PrimeiraTela()
+                    2 -> SegundaTela()
+                }
+            }
+
+            Row (
+                Modifier
+
+                    .height(70.dp)
+                    .fillMaxWidth()
+                    .background(Color(0xFF1AB6FF)),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+
+            ){
+                Button(onClick = { tela.value = 1}) {
+                    Text(text = "Tela 1", fontSize = 30.sp)
+                }
+                Button(onClick = { tela.value = 2}) {
+                    Text(text = "Tela 2", fontSize = 30.sp)
+                }
+            }
+        }
+
+    }
+
+}
+
+
+
---
+++ app/src/main/java/br/edu/up/planner/ui/screens/SegundaTela.kt
@@ -0,0 +1,14 @@
+package br.edu.up.planner.ui.screens
+
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SegundaTela() {
+    Surface {
+        Text(text = "Segunda Tela", fontSize = 50.sp)
+    }
+
+}
